f44b3c7073e1187b131bd9d9498bf378b5e3e6b7
Steven De Avila <stevejd88@gmail.com>
2021-01-15 10:43:14 -0600

    create and add decorators and controller to Login route

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
 import cookieSession from 'cookie-session';
 
+import { router as controllerRouter } from './controllers/decorators/controller';
+import './controllers/LoginController';
+
 const app = express();
 
 // app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['lasso'] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,26 +1,20 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
 }
 
-const router = Router();
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
 
-router.get('/login', (req: Request, res: Response) => {
-  res.send(`
-    <form method="POST">
-      <div>
-        <label>Email</label>
-        <input name="email" />
-      </div>
-      <div>
-        <label>Password</label>
-        <input name="password" type="password" />
-      </div>
-      <button>Submit</button>
-    </form>
-  `);
-});
+  res.status(403);
+  res.send('Not permitted');
+}
+
+const router = Router();
 
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
@@ -56,6 +50,10 @@ router.get('/', (req: Request, res: Response) => {
 router.get('/logout', (req: Request, res: Response) => {
   req.session = undefined;
   res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged in user!');
 });
 
 export { router };
